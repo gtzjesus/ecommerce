3f47efbfde682fd2948a9d8b59367b40ad6fc6e5
gtz.jesus <gtz.jesus@outlook.com>
2024-09-11 16:02:04 -0600

    File structure + header implementation

--- components/header/header.js
+++ components/header/header.js
@@ -2,28 +2,32 @@ import Link from 'next/link';
 import styles from './header.module.css';
 import Image from 'next/image';
 import logo from '../../assets/icons/world.webp';
+import search from '../../assets/icons/search.webp';
+import account from '../../assets/icons/account.webp';
+import bag from '../../assets/icons/bag.webp';
+import menu from '../../assets/icons/menu.webp';
 
 export default function Header() {
   return (
     <div className={styles.header}>
       <ul className={styles.ul}>
-        <Link  href="/">
+        <Link href="/">
           <Image className={styles.logo} src={logo} alt="ecommerce" priority />
         </Link>
       </ul>
       <ul className={styles.ul}>
-        <li>
-          <a>menu</a>
-        </li>
-        <li>
-          <a>menu</a>
-        </li>
-        <li>
-          <a>menu</a>
-        </li>
-        <li>
-          <a>menu</a>
-        </li>
+        <Link href="/search">
+          <Image className={styles.icon} src={search} alt="search" priority />
+        </Link>
+        <Link href="/account">
+          <Image className={styles.icon} src={account} alt="account" priority />
+        </Link>
+        <Link href="/bag">
+          <Image className={styles.icon} src={bag} alt="bag" priority />
+        </Link>
+        <Link href="/menu">
+          <Image className={styles.icon} src={menu} alt="menu" priority />
+        </Link>
       </ul>
     </div>
   );
