160aeafa240e0654aece848be95429d588de26dc
gtz.jesus <gtz.jesus@outlook.com>
2024-09-11 15:35:30 -0600

    File structure + header implementation

--- components/header/header.js
+++ components/header/header.js
@@ -1,14 +1,26 @@
+import Link from 'next/link';
 import styles from './header.module.css';
+import Image from 'next/image';
+import logo from '../../assets/icons/world.webp';
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      <ul>
-        <li>
-          <a>logo</a>
-        </li>
+      <ul className={styles.ul}>
+        <Link  href="/">
+          <Image className={styles.logo} src={logo} alt="ecommerce" priority />
+        </Link>
       </ul>
-      <ul>
+      <ul className={styles.ul}>
+        <li>
+          <a>menu</a>
+        </li>
+        <li>
+          <a>menu</a>
+        </li>
+        <li>
+          <a>menu</a>
+        </li>
         <li>
           <a>menu</a>
         </li>
