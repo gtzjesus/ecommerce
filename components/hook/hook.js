b68969c8d733aedb71d06a12fc9a3f9e6666ff11
gtz.jesus <gtz.jesus@outlook.com>
2024-09-12 01:59:43 -0600

    Hook component addition

--- components/hook/hook.js
+++ components/hook/hook.js
@@ -4,10 +4,15 @@ export default function Hook() {
   return (
     <div className={styles.hook}>
       <h2 className={styles.h2}>Tan Nike Sneakers</h2>
+      <br />
       <h1 className={styles.h1}>Style Your World</h1>
+      <br />
+
       <p className={styles.p}>
         A lightweight daily designed for every fashionist.
       </p>
+      <br />
+      <br />
       <Button text="Shop" />
     </div>
   );
